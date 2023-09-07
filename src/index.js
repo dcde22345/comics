43d073d12b8a44b931037a4c5d565eb698248ed8
dcde22345 <dcde22345@gmail.com>
2023-09-07 12:09:40 +0800

    second

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import './styles/style.css'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
+import "normalize.css"
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
